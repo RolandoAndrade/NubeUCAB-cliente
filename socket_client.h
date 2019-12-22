1013617f7d09fa7d48e4e562885fdee46008d191
rolandoandrade <rolandoandradefernandez@gmail.com>
2019-12-22 19:21:41 -0400

    Constructores

--- socket_client.h
+++ socket_client.h
@@ -1,13 +1,42 @@
 #include "socket.h"
 #include "socket_exceptions.h"
+#include "commands.h"
 
 class ClientSocket : private Socket
 {
 	public:
-		ClientSocket();
-		ClientSocket(int, int);
-		ClientSocket(std::string, int);
-		~ClientSocket();
+		ClientSocket()
+		{
+
+		}
+		ClientSocket(int host, int port)
+		{
+			if(!Socket::create())
+			{
+				throw SocketException(strerror(errno));
+			}
+			if(!Socket::connect(host,port))
+			{
+				throw SocketException(strerror(errno));
+			}
+		}
+
+		ClientSocket(string host, int port)
+		{
+			if(!Socket::create())
+			{
+				throw SocketException(strerror(errno));
+			}
+			int ip = lookup(ip_address);
+			if(!Socket::connect(ip,port)){
+				throw SocketException(strerror(errno));
+			}
+		}
+		~ClientSocket()
+		{
+			Socket::close();
+		}
+		
 		ClientSocket& operator << (std::string&);
 		ClientSocket& operator >> (std::string&);
 		void close();
